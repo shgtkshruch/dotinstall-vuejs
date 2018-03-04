14fc135c40fa8f21bf4a13844506fddfe3800d45
shgtkshruch <shgtk.shruch@gmail.com>
2018-03-04 11:06:01 +0900

    Clean

--- js/main.js
+++ js/main.js
@@ -1,48 +1,8 @@
 (() => {
 'use strict';
 
-  var vm = new Vue({
+  const app = new Vue({
     el: '#app',
-    data: {
-      newItem: '',
-      todos: [],
-    },
-    watch: {
-      todos: {
-        handler() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-        },
-        deep: true,
-      },
-    },
-    mounted() {
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    },
-    methods: {
-      addItem() {
-        const item = {
-          title: this.newItem,
-          isDone: false,
-        }
-        this.todos.push(item);
-        this.newItem = '';
-      },
-      deleteItem(i) {
-        if (confirm('are you sure?')) {
-          this.todos.splice(i, 1);
-        }
-      },
-      purge() {
-        if (!confirm('delete finished?')) {
-          return;
-        }
-        this.todos = this.remaining;
-      }
-    },
-    computed: {
-      remaining() {
-        return this.todos.filter(todo => !todo.isDone)
-      }
-    }
   });
+
 })();
