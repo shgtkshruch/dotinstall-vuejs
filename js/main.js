b06b993d04be1fad204e34ecbd9b97038bb5ff30
shgtkshruch <shgtk.shruch@gmail.com>
2018-03-04 11:15:57 +0900

    Props

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@
 'use strict';
 
   const likeComponent = Vue.extend({
+    props: {
+      message: {
+        type: String,
+        default: 'Like',
+      },
+    },
     data() {
       return {
         count: 0
       };
     },
-    template: '<button @click="countUp">Like {{ count }}</button>',
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
     methods: {
       countUp() {
         this.count++;
